6a2dd99514a4f487570ec2b6073f7ec59cde0ba9
root <yinjunok@foxmail.com>
2024-05-15 14:53:55 +0800

    i18n 优化

--- src/i18n/locales/en-US.ts
+++ src/i18n/locales/en-US.ts
@@ -1,8 +1,11 @@
 export default {
   '新增': 'add',
   '详情': 'detail',
-  '列表页': 'List page',
+  '产品列表': 'Product List',
   '控制面板': 'Control Panel',
   '计数器': 'count: {{count}}',
   '表单页': 'Form Page',
+  '数据': 'data',
+  '分析': 'analyze',
+  '列表': 'List',
 }
